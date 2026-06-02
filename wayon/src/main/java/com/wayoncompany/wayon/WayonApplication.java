5c232722812f10a1ee6789374f5d4fb3ccf802af
Brandon Carlos <brandon_marzo@hotmail.com>
2026-06-02 20:30:28 -0300

    Initial commit

---
+++ wayon/src/main/java/com/wayoncompany/wayon/WayonApplication.java
@@ -0,0 +1,13 @@
+package com.wayoncompany.wayon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayonApplication.class, args);
+	}
+
+}
