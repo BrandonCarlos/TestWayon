49f15777398390ff1257116c616dd45bf5751ad6
Brandon Carlos <brandon_marzo@hotmail.com>
2026-06-02 21:44:39 -0300

    TransferControllerTest layer done

---
+++ wayon/src/test/java/com/wayoncompany/wayon/entrypoint/rest/TransferControllerTest.java
@@ -0,0 +1,55 @@
+package com.wayoncompany.wayon.entrypoint.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wayoncompany.wayon.application.dto.TransferRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TransferController.class)
+public class TransferControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void shouldValidateRequest()
+            throws Exception {
+
+        TransferRequest request =
+                new TransferRequest();
+
+        request.setSourceAccount(
+                "123");
+
+        request.setDestinationAccount(
+                "456");
+
+        request.setAmount(
+                BigDecimal.valueOf(1000));
+
+        request.setTransferDate(
+                LocalDate.now().plusDays(5));
+
+        mockMvc.perform(
+                        post("/api/transfers")
+                                .contentType(
+                                        "application/json")
+                                .content(
+                                        mapper.writeValueAsString(
+                                                request))
+                )
+                .andExpect(
+                        status().isBadRequest()
+                );
+    }
+}
