3ce3c3f53a31d2cf7707c16ee0d7a68433bd3201
Brandon Carlos <brandon_marzo@hotmail.com>
2026-06-02 21:52:21 -0300

    fixed TransferRepositoryAdapterTest layer

--- wayon/src/test/java/com/wayoncompany/wayon/infrastructure/persistence/TransferRepositoryAdapterTest.java
+++ wayon/src/test/java/com/wayoncompany/wayon/infrastructure/persistence/TransferRepositoryAdapterTest.java
@@ -2,23 +2,18 @@ package com.wayoncompany.wayon.infrastructure.persistence;
 
 import com.wayoncompany.wayon.infrastructure.persistence.repository.JpaTransferRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 public class TransferRepositoryAdapterTest {
-    private final JpaTransferRepository repository;
-
-    TransferRepositoryAdapterTest(
-            JpaTransferRepository repository) {
-
-        this.repository = repository;
-    }
+    @Autowired
+    private JpaTransferRepository repository;
 
     @Test
     void shouldLoadRepository() {
-
         assertNotNull(repository);
     }
 }
